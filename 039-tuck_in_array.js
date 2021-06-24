ecf84fdad49df4edd3a1cc9cddd50a1f533d11bb
deepen77 <przemek.starostka@yahoo.com>
2021-06-24 19:49:57 +0100

    039-tuck_in_array

---
+++ 039-tuck_in_array.js
@@ -0,0 +1,43 @@
+/*
+Day-by-Day-Challenge
+Exercice 037 - "are the numbers equal"
+
+Task
+------------
+
+Create a function that takes two arrays and insert the second array in the middle of the first array.
+
+Examples
+tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9]) ➞ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+tuckIn([15,150], [45, 75, 35]) ➞ [15, 45, 75, 35, 150]
+
+tuckIn([[1, 2], [5, 6]], [[3, 4]]) ➞ [[1, 2], [3, 4], [5, 6]]
+
+*/
+
+// function tuckIn(arr1, arr2) {
+//   arr2.push(arr1[1]);
+//   arr2.unshift(arr1[0]);
+//   return arr2;
+// }
+
+function tuckIn(arr1, arr2) {
+  return [arr1[0], ...arr2, arr1[1]];
+}
+
+function verify(input, goal) {
+  if (input === goal) {
+    console.log("Congratulation!");
+  } else {
+    console.log(`Sorry, expected - ${goal}, received - ${input}`);
+  }
+}
+
+
+
+
+
+verify(tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9]),[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+verify(tuckIn([15,150], [45, 75, 35]),[15, 45, 75, 35, 150]);
+verify(tuckIn([[1, 2], [5, 6]], [[3, 4]]),[[1, 2], [3, 4], [5, 6]]);
