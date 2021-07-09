e72d3f7253d727f08e22955cfdc7a83e3d4f7e90
deepen77 <przemek.starostka@yahoo.com>
2021-07-09 21:19:39 +0100

    054-template_string

---
+++ 054-template_string.js
@@ -0,0 +1,29 @@
+/*
+Day-by-Day-Challenge
+Exercice 053 - "Template string"
+
+Task
+------------
+Description:
+
+Create a template string based on the info variable
+*/
+
+
+const name = "Smbd Test";
+const age = 35;
+const role = "Admin";
+
+const info =
+  name +
+  " has a role of " +
+  role +
+  " and is born in year " +
+  (new Date().getFullYear() - age);
+
+const infoTemplateString = `${name} has a role of ${role} and is born in year ${
+  new Date().getFullYear() - age
+}`;
+
+console.log(info);
+console.log(infoTemplateString);
