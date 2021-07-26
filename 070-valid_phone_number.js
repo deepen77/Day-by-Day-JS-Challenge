d27dd5a449c146fa56bdd3b1f7099d889f12e989
deepen77 <przemek.starostka@yahoo.com>
2021-07-26 20:10:36 +0100

    070-valid_phone_number

---
+++ 070-valid_phone_number.js
@@ -0,0 +1,43 @@
+/*
+Day-by-Day-Challenge
+Exercice 070 - "Valid phone number"
+
+Task
+------------
+
+Description:
+Write a function that accepts a string, and returns true if it is in the form of a phone number. 
+Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
+Only worry about the following format:
+(123) 456-7890 (don't forget the space after the close parentheses) 
+Examples:
+validPhoneNumber("(123) 456-7890")  =>  returns true
+validPhoneNumber("(1111)555 2345")  => returns false
+validPhoneNumber("(098) 123 4567")  => returns false
+*/
+
+//solution1
+
+function validPhoneNumber(phoneNumber) {
+  return /^\(\d{3}\)\ \d{3}\-\d{4}$/.test(phoneNumber);
+}
+
+
+//solution2
+
+function validPhoneNumber2(str) {
+  if (str.length !== 14) {
+    return false;
+  }
+  if (str[4] !== ")") {
+    return false;
+  }
+  if (str[5] !== " ") {
+    return false;
+  }
+  if (str[9] !== "-") {
+    return false;
+  } else {
+    return true;
+  }
+}
